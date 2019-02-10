c970d6da03d87f63bc379131db06630a4d6fb42c
Anthony Rinaldo <rinaldaj@clarkson.edu>
2019-02-10 08:44:39 -0500

    I'm really tired

--- display.js
+++ display.js
@@ -8,7 +8,7 @@ function populateStats(person){
 	let cons = document.getElementById("cons")
 	let consMod = (person.constitution - 10) / 2
 	let intel = document.getElementById("intel")
-	let intelMod = (person.intelliligence - 10) / 2
+	let intelMod = (person.intelligence - 10) / 2
 	let wis = document.getElementById("wis")
 	let wisMod = (person.wisdom - 10) / 2
 	let cha = document.getElementById("cha")
@@ -20,7 +20,7 @@ function populateStats(person){
 	str.innerHTML = person.strength + "(" + (strMod > 0? "+":"")+ strMod.toString() + ")"
 	dex.innerHTML = person.dexterity + "(" + (dexMod > 0? "+":"")+ dexMod + ")"
 	cons.innerHTML = person.constitution + "(" + (consMod > 0? "+":"")+ consMod + ")"
-	intel.innerHTML = person.intelliligence + "(" + (intelMod > 0? "+":"")+ intelMod + ")"
+	intel.innerHTML = person.intelligence + "(" + (intelMod > 0? "+":"")+ intelMod + ")"
 	wis.innerHTML = person.wisdom + "(" + (wisMod > 0? "+":"")+ wisMod + ")"
 	cha.innerHTML = person.charisma + "(" + (chaMod > 0? "+":"")+ chaMod + ")"
 	nameBox.innerHTML = person.name
@@ -28,6 +28,34 @@ function populateStats(person){
 	for (var i =0; i< person.class.length;i++){
 		let tmp = document.createTextNode(person.class[i].class + ":"+ person.class[i].level +" ")
 		classBox.appendChild(tmp)
+	}
+	
+	let prof = document.getElementById("profbox")
+	for(var i =0;i<person.proficiencies.length;i++){
+		prof.appendChild(document.createTextNode(person.proficiencies[i] + ", "))
+	}
+	let langs = document.getElementById("langbox")
+	for(var i =0;i<person.languages.length;i++){
+		langs.appendChild(document.createTextNode(person.languages[i] + ", "))
+	}
+	let kiva = document.getElementById("feats")
+	for(var i =0;i<person.feats.length;i++){
+		let t = document.createElement("h2")
+		t.innerHTML = person.feats[i].feat
+		let box = document.createElement("div")
+		box.appendChild(document.createTextNode(person.feats[i].description))
+		kiva.appendChild(t)
+		kiva.appendChild(box)
+	}
+	let stabbystick = document.getElementById("inventory")
+	for (var i =0; i < person.inventory.length;i++){
+		let row = stabbystick.insertRow(1)
+		let iname = row.insertCell(0)
+		let effect = row.insertCell(1)
+		let price = row.insertCell(-1)
+		iname.innerHTML = person.inventory[i].item_name + " "
+		price.innerHTML = person.inventory[i].cost + " "
+		effect.innerHTML = (typeof person.inventory[i].damage !== 'undefined'? person.inventory[i].damage[0].damage_dice + " " + person.inventory[i].damage[0].damage_type : person.inventory[i].ac.base) + " "
 	}
 
 }
--- makeChar.js
+++ makeChar.js
@@ -7,38 +7,38 @@ function finnaMakeChar(){
 	let stats = JSON.parse(localStorage.getItem('stats'))
 	let desc = JSON.parse(localStorage.getItem('desc'))
 	let inv = localStorage.getItem('inv').split(',')
-	let play = {name: desc.name, gender: desc.gender, alignment:desc.alignment,background:"",height:desc.height,languages:desc.lang,weight:desc.weight,proficiencies:[],feats:[], class: [{level:1,class:localStorage.getItem('chosenClass')}], inventory:[], strength: stats.str, dexterity:stats.dex,wisdom:stats.wis,intelligence:stats.intel,constitution:stats.cons,charisma:stats.cha, race: ""}
+	let play = {name: desc.name, gender: desc.gender, alignment:desc.alignment,background:"",height:desc.height,languages:desc.lang,weight:desc.weight,proficiencies:[],feats:[], class: [{level:1,class:localStorage.getItem('chosenClass')}], inventory:[], strength: Number(stats.str), dexterity:Number(stats.dex),wisdom:Number(stats.wis),intelligence:Number(stats.intel),constitution:Number(stats.cons),charisma:Number(stats.cha), race: ""}
 	play.proficiencies.push(classProf)
 	play.proficiencies.push(morProf)
 	let race = loadDir("./races")[raceNum]
 	for(var i = 0; i<race.languages.length;i++){
 		play.languages.push(race.languages[i])
 	}
-	for (var i=0; i < race.feats;i++){
+	for (var i=0; i < race.feats.length;i++){
 		play.feats.push(race.feats[i])
 		
 	}
-	for (var i=0; i < race.armor_proficiencies;i++){
+	for (var i=0; i < race.armor_proficiencies.length;i++){
 		play.proficiencies.push(race.armor_proficiencies[i])
 		
 	}
-	for (var i=0; i < race.weapon_proficiencies;i++){
+	for (var i=0; i < race.weapon_proficiencies.length;i++){
 		play.proficiencies.push(race.weapon_proficiencies[i])
 		
 	}
-	for (var i=0; i < race.ability_score_increase;i++){
+	for (var i=0; i < race.ability_score_increase.length;i++){
 		if ( race.ability_score_increase[i].stat === "charisma"){
-			play.charisma += race.ability_score_increase[i].mod
+			play.charisma += Number(race.ability_score_increase[i].mod)
 		} else if ( race.ability_score_increase[i].stat === "wisdom"){
-			play.wisdom += race.ability_score_increase[i].mod
+			play.wisdom += Number(race.ability_score_increase[i].mod)
 		} else if ( race.ability_score_increase[i].stat === "dexterity"){
-			play.dexterity += race.ability_score_increase[i].mod
+			play.dexterity += Number(race.ability_score_increase[i].mod)
 		} else if ( race.ability_score_increase[i].stat === "strength"){
-			play.strength += race.ability_score_increase[i].mod
+			play.strength += Number(race.ability_score_increase[i].mod)
 		} else if ( race.ability_score_increase[i].stat === "intelligence"){
-			play.intelligence += race.ability_score_increase[i].mod
+			play.intelligence += Number(race.ability_score_increase[i].mod)
 		} else if ( race.ability_score_increase[i].stat === "constitution"){
-			play.constitution += race.ability_score_increase[i].mod
+			play.constitution += Number(race.ability_score_increase[i].mod)
 		}
 		
 	}
@@ -54,7 +54,9 @@ function finnaMakeChar(){
 	
 	let eList = loadDir("./items")
 	inv.forEach(function (i) {
+		if ( !isNaN(i)){
 		play.inventory.push(eList[i])
+		}
 	})
 
 	//Do last
